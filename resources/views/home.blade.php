afdb103e7f057b720070190c7751ef74318acfb6
Ali Dindar <ali_dindar@live.be>
2020-08-13 00:47:48 +0200

    display multiple products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,27 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        <h2>Product</h2>
+                        <div class="row">
+                            <!-- Create a loop for each product -->
+                            @foreach ($allProduct as $products)
+                                <div class="col-4">
+                                    <div class="card" style="width: 18rem;">
+                                        <img class="card-img-top" src="default-freelance.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                            <!-- display the name of the product given by the factory -->
+                                            <h5 class="card-title">{{$products->name}}</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Select freelance</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
